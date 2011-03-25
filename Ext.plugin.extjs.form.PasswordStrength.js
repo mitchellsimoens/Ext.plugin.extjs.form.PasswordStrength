633918b04fa693052262603728f75d5b17be1af7
Mitchell Simoens <ms.simoens@gmail.com>
2011-03-25 12:24:36 -0400

    Initial commit

---
+++ Ext.plugin.extjs.form.PasswordStrength.js
@@ -0,0 +1,103 @@
+Ext.define("Ext.plugin.extjs.form.PasswordStrength", {
+	extend : "Ext.AbstractPlugin",
+	alias  : "plugin.passwordstrength",
+	colors : ["C11B17", "FDD017", "4AA02C", "6AFB92", "00FF00"],
+
+	init: function(cmp) {
+		var me = this;
+
+		cmp.on("change", me.onFieldChange, me);
+	},
+
+	onFieldChange: function(field, newVal, oldVal) {
+		if (newVal === "") {
+			field.inputEl.setStyle({
+				"background-color" : null,
+				"background-image" : null
+			});
+			field.score = 0;
+			return ;
+		}
+		var me    = this,
+			score = me.scorePassword(newVal);
+
+		field.score = score;
+
+		me.processValue(field, score);
+	},
+
+	processValue: function(field, score) {
+		var me     = this,
+			colors = me.colors,
+			color;
+
+		if (score < 16) {
+			color = colors[0]; //very weak
+		} else if (score > 15 && score < 25) {
+			color = colors[1]; //weak
+		} else if (score > 24 && score < 35) {
+			color = colors[2]; //mediocre
+		} else if (score > 34 && score < 45) {
+			color = colors[3]; //strong
+		} else {
+			color = colors[4]; //very strong
+		}
+
+		field.inputEl.setStyle({
+			"background-color" : "#" + color,
+			"background-image" : "none"
+		});
+	},
+
+	scorePassword: function(passwd) {
+		var score = 0;
+
+		if (passwd.length < 5) {
+			score += 3;
+		} else if (passwd.length > 4 && passwd.length < 8) {
+			score += 6;
+		} else if (passwd.length > 7 && passwd.length < 16) {
+			score += 12;
+		} else if (passwd.length > 15) {
+			score += 18;
+		}
+
+		if (passwd.match(/[a-z]/)) {
+			score += 1;
+		}
+
+		if (passwd.match(/[A-Z]/)) {
+			score += 5;
+		}
+
+		if (passwd.match(/\d+/)) {
+			score += 5;
+		}
+
+		if (passwd.match(/(.*[0-9].*[0-9].*[0-9])/)) {
+			score += 5;
+		}
+
+		if (passwd.match(/.[!,@,#,$,%,^,&,*,?,_,~]/)) {
+			score += 5;
+		}
+
+		if (passwd.match(/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/)) {
+			score += 5;
+		}
+
+		if (passwd.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+			score += 2;
+		}
+
+		if (passwd.match(/([a-zA-Z])/) && passwd.match(/([0-9])/)) {
+			score += 2;
+		}
+
+		if (passwd.match(/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/)) {
+			score += 2;
+		}
+
+		return score;
+	}
+});
